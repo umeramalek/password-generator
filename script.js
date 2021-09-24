256f7ccc239e247712872bd68864f5b7d66d7a37
Umera <umeramalek796@gmail.com>
2021-09-23 19:47:21 -0700

    js 2 line code

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-// variable for input 
-var password = document.getElementById("passwordBox")
-
-// variable for characters
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
-var numbers = "0123456789"
-var specialCharacters = "!@#$%^&-+*><"
-
-// to activate the generate button
-for (var i = 0; i <= characterLenght; i++) {
-    var randomNumber = Math.floor(Math.random() * uppperLetters.length);
-    password += upperLetters.substring(randomNumber, randomNumber +1);
-   }
-   document.getElementById("password").value = password;
+var generateBtn = document.querySelector("generate");
 
 
-   // prompt a message asking for character lenght
-let characterLenght = prompt("How many characters do you want your password?");
-// setting a max and min for the password 8-128 characters and if more or less than that, need to decide a proper lenght
-if (characterLenght < 8 || characterLenght >= 128) {
-    alert("value should be between 8-128 characters ")
-
-
-
-
-// activating the copy button 
-const copyPwd = document.getElementById("passwordBox");
-copyPwd.addEventListener("click"); {
-    password.select();
-    //   document.execCommand("copy");
-     alert("Password Copied");
-}   
+generateBtn.addEventListener('click', function(){
+    window.alert("it works");
+});
